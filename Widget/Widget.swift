d1f2de6bdec7fe191c322851336bf9fb077e7b79
Yuuki <982380365@qq.com>
2020-06-28 16:59:28 +0800

    feat(widget): add widget target

---
+++ Widget/Widget.swift
@@ -0,0 +1,64 @@
+//
+//  Widget.swift
+//  Widget
+//
+//  Created by Yuuki on 2020/6/28.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    public typealias Entry = SimpleEntry
+
+    public func snapshot(with context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    public func timeline(with context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    public let date: Date
+}
+
+struct PlaceholderView : View {
+    var body: some View {
+        Text("Placeholder View")
+    }
+}
+
+struct WidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct Widget: Widget {
+    private let kind: String = "Widget"
+
+    public var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider(), placeholder: PlaceholderView()) { entry in
+            WidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
