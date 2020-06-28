a37a6602abc928520fd3468ef24ee9e3ada313eb
Yuuki <982380365@qq.com>
2020-06-28 16:57:40 +0800

    feat(project): init project

---
+++ WidgetDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WidgetDemo
+//
+//  Created by Yuuki on 2020/6/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
